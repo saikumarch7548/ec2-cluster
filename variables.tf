c541ec9324fac0b97b1cd00e04051b630b2cfeaa
saikumar chukkapalli <34895425+saikumarch7548@users.noreply.github.com>
2019-04-11 00:44:01 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  default = "aws-ecs-poc"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "dockerimg" {
+  default = "bctux/web-server-info"
+}
+
+variable "ami" {
+  default = {
+    us-east-1 = "ami-aff65ad2"
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "max_instances" {
+  default = 2
+}
+
+variable "desired_instances" {
+  default = 2
+}
+
+variable "lb_port" {
+  default = 80
+}
+
+variable "host_port" {
+  default = 0
+}
+
+variable "docker_port" {
+  default = 8080
+}
